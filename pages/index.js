d31219c54ba110fc428efac80ec3585ddd088e1d
pjames <pjames@gmail.com>
2026-05-08 10:23:29 +0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Hello World</h1>
+      <p>Next.js app is running on port 3000</p>
+    </main>
+  );
+}
