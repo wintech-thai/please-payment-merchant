e6adda3a37c9fdedf2b63360eda8e58b66ad3ec9
auttz <auttzmargaretz@gmail.com>
2026-06-04 16:49:20 +0700

    Fix Error From Invite User

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get('accessToken')?.value
 
   if (
-    pathname.startsWith('/api/auth/') ||
+    pathname.startsWith('/api/') ||
     pathname.startsWith('/_next/') ||
     pathname.startsWith('/favicon')
   ) {
